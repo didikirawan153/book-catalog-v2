25ce92c125d910b4bb1d0fc731c5335d5a54012e
didikirawan153 <didikirawan153@gmail.com>
2023-09-07 22:34:04 +0700

    spring configuration processor

--- src/main/java/com/practicedidik/catalog/service/impl/GreetingServiceImpl.java
+++ src/main/java/com/practicedidik/catalog/service/impl/GreetingServiceImpl.java
@@ -1,19 +1,28 @@
 package com.practicedidik.catalog.service.impl;
 
+import java.util.TimeZone;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import com.practicedidik.catalog.config.AppConfig;
 import com.practicedidik.catalog.service.GreetingService;
 
 @Service
 public class GreetingServiceImpl implements GreetingService {
 
-	@Value("${welcome.text}")
-	private String welcomeText;
+	private AppConfig appConfig;
+	
+	public GreetingServiceImpl(AppConfig appConfig) {
+		super();
+		this.appConfig = appConfig;
+	}
 
 	@Override
 	public String sayGreeting() {
-		return welcomeText;
+		TimeZone timeZone = TimeZone.getTimeZone(appConfig.getTimezone());
+		return appConfig.getWelcomeText()+", Our timezone : "+timeZone.getDisplayName()+
+				", Our currency : "+appConfig.getCurrency();
 	}
 	
 }
