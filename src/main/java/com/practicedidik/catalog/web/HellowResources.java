661032d4db46ab5450b193cd7118483b0d99fb18
didikirawan153 <didikirawan153@gmail.com>
2023-09-11 15:16:27 +0700

    Spring boot logging

--- src/main/java/com/practicedidik/catalog/web/HellowResources.java
+++ src/main/java/com/practicedidik/catalog/web/HellowResources.java
@@ -1,5 +1,7 @@
 package com.practicedidik.catalog.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +9,8 @@ import com.practicedidik.catalog.service.GreetingService;
 
 @RestController
 public class HellowResources {
+	
+	Logger log = LoggerFactory.getLogger(HellowResources.class);
 	
 	private GreetingService greetingService;
 	
@@ -19,6 +23,11 @@ public class HellowResources {
 
 	@GetMapping(value = "/hello")
 	public String helloWorld() {
+		log.trace("Ini log TRACE");
+		log.debug("Ini log DEBUG");
+		log.info("Ini log INFO");
+		log.warn("Ini log WARN");
+		log.error("Ini log ERROR");
 		return greetingService.sayGreeting();
 	}
 }
