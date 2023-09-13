0cc843110452bcf1e192be25311f873a302be5ff
didikirawan153 <didikirawan153@gmail.com>
2023-09-13 15:59:16 +0700

    Logging dengan lombok

--- src/main/java/com/practicedidik/catalog/web/BookResources.java
+++ src/main/java/com/practicedidik/catalog/web/BookResources.java
@@ -1,5 +1,6 @@
 package com.practicedidik.catalog.web;
 
+import org.springframework.util.StopWatch;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,9 @@ import com.practicedidik.catalog.domain.dto.BookDetailDto;
 import com.practicedidik.catalog.service.BookService;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @AllArgsConstructor
 @RestController
 public class BookResources {
@@ -17,6 +20,11 @@ public class BookResources {
 	
 	@GetMapping("/book/{bookId}")
 	public BookDetailDto getBookById(@PathVariable("bookId") Long id) {
-		return bookService.findBookById(id);
+		StopWatch stopWatch = new StopWatch();
+		log.info("Start findBookById with ID : "+id);
+		stopWatch.start();
+		BookDetailDto result = bookService.findBookById(id);
+		log.info("Finish findBookById with ID : "+id+" with time : "+stopWatch.getTotalTimeMillis());
+		return result;
 	}
 }
